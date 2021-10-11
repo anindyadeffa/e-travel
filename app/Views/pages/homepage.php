9bd5f7c1c39c952942b5d13de5097dab76f182c8
syrznh <saruriansyah@gmail.com>
2021-10-11 14:21:32 +0700

    Syariz After

--- app/Views/pages/homepage.php
+++ app/Views/pages/homepage.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </meta>
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <title>Homepage</title>
+    <title>Welcome To Travel </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('mainpages/css/styles.css'); ?>">
