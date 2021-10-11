79cf6a29db14c1d40033b2db37cde698149ae688
Bramatrihadi <bramatrihadisyapoetra@gmail.com>
2021-10-11 13:24:10 +0700

    bramz after

--- app/Views/pages/mainpages/pages/index.php
+++ app/Views/pages/mainpages/pages/index.php
@@ -5,7 +5,7 @@
     <div class="d-flex">
         <div class="col-xl-6 my-5">
             <h1 style="color: #F5C234; font-size: 42px; font-weight: 700;">Explore the Wonderland Indonesia</h1>
-            <p>You will see beautiful moment you never seen before</p>
+            <p>You will see beautiful moment you never seen before dan after</p>
         </div>
         <div class="col-xl-6">
             <img src="/mainpages/img/header.png" class="w-100 h-100" alt="">
