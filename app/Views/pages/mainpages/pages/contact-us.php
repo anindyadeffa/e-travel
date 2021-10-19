64393c47202a47a07406d76049ca1670aadb2eb9
anindyadeffanabila@gmail.com <66814960+anindyadeffa@users.noreply.github.com>
2021-10-19 23:40:50 +0700

    Deffa | edit contact us page

--- app/Views/pages/mainpages/pages/contact-us.php
+++ app/Views/pages/mainpages/pages/contact-us.php
@@ -1,7 +1,7 @@
 <?= $this->extend('/pages/mainpages/layouts/master') ?>
 <?= $this->section('content') ?>
 
-<div class="gradien">
+<!-- <div class="gradien">
     <div class="container-fluid" style="padding-top: 50px; padding-bottom: 50px;">
         <div class="container">
             <h2 style="color:#F5C234; font-weight: 700;" class="contact text-center">CONTACT US</h2>
@@ -27,7 +27,7 @@
                         <form>
                             <div class="col mb-4">
                                 <label for="messages">Message</label>
-                                <textarea class="form-control" style="height: 88px;" id="messages" rows="3" placeholder="Write Your Message Here..."></textarea>
+                                <textarea class="form-control" style="height: 88px;" id="messages" rows="" placeholder="Write Your Message Here..."></textarea>
                             </div>
                             <div class="col d-grid gap-2 d-md-flex justify-content-md-end">
                                 <button class="btn-block btn-login" style="width: 173px; height: 44px;">Send</button>
@@ -63,5 +63,69 @@
             </div>
         </div>
     </div>
+</div> -->
 
-    <?= $this->endSection() ?>
+<section>
+    <div class="container my-5">
+        <h2 class="font-weight-bold text-center text-uppercase" style="color: #F5C234;">Contact Us</h2>
+        <div class="card mt-5" style="border-radius: 10px;">
+            <div class="card-body my-4">
+                <div class="row justify-content-center">
+                    <div class="col-xl-5 mx-3 my-2">
+                        <div class="text-center">
+                            <img src="<?php echo base_url('/mainpages/img/message.png'); ?>" class="w-75 h-75" alt="...">
+                        </div>
+                        <div class="mt-5 mb-3">
+                            <h5 class="font-weight-bold">Our Address</h5>
+                            <p>
+                                12345. West Jakarta St. South Meruya,
+                                <br>
+                                Indonesia
+                            </p>
+                        </div>
+                        <div class="mb-3">
+                            <h5 class="font-weight-bold">Our Contact</h5>
+                            <p>
+                                +[12] 3456 7890
+                            </p>
+                        </div>
+                        <div class="mt-3">
+                            <h5 class="font-weight-bold">Our Email</h5>
+                            <p>
+                                support@travelin.com
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-xl-5 mx-3 my-2">
+                        <form>
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="name" class="form-control" id="name" placeholder="Enter your name ...">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">E-mail Address</label>
+                                <input type="email" class="form-control" id="email" placeholder="Enter your e-mail address ...">
+                            </div>
+                            <div class="form-group">
+                                <label for="phone">Phone Number</label>
+                                <input type="phone" class="form-control" id="phone" placeholder="Enter your phone number ...">
+                            </div>
+                            <div class="form-group">
+                                <label for="message">Message</label>
+                                <textarea class="form-control" id="message" rows="6" placeholder="Enter your message here ..."></textarea>
+                            </div>
+                            <div class="text-right d-block">
+                                <button href="#" type="submit" class="btn btn-login px-5 mr-auto">
+                                    <p class="mb-0 font-weight-bold">SEND</p>
+                                </button>
+                            </div>
+                            <!-- <button type="submit" class="btn btn-primary">Submit</button> -->
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?= $this->endSection() ?>
