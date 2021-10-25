96bd7aef35a0eb52d0e9c8674f8938deec002904
anindyadeffanabila@gmail.com <66814960+anindyadeffa@users.noreply.github.com>
2021-10-25 15:53:21 +0700

    Deffa | add verification page

---
+++ app/Controllers/Verification.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Verification extends BaseController
+{
+    public function index()
+    {
+        return view('/pages/mainpages/pages/verification');
+    }
+}
---
+++ app/Views/pages/mainpages/pages/verification.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </meta>
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>Travel.in</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"> -->
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> -->
+    <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
+    <link href="<?php echo base_url('/mainpages/css/styles.css'); ?>" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url('/mainpages/libraries/font-awesome/css/font-awesome.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('/mainpages/libraries/perfect-scrollbar/css/perfect-scrollbar.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('/mainpages/libraries/jquery-switchbutton/jquery.switchButton.css') ?>" rel="stylesheet">
+</head>
+
+<body class="Poppins">
+    <!-- navbar -->
+    <div class="container">
+        <nav class="row navbar navbar-expand-xl bg-white">
+            <div class="col-xl-12 text-left">
+                <a href="/" style="text-decoration: none; color: #98c8db; font-size: 25px; font-weight: 700;">TRAVEL.IN</a>
+            </div>
+        </nav>
+        <div class="text-center">
+            <img src="<?php echo base_url('/mainpages/img/message.png'); ?>" class="w-25 h-25 mt-5 pt-5" alt="...">
+            <h6 class="text-center mt-5" style="color: #F5C234;">
+                Pembayaran akan segera di proses, <br>
+                untuk informasi lebih lanjut akan di informasikan melalui e-mail!
+            </h6>
+        </div>
+    </div>
+
+    <script src="<?php echo base_url('/mainpages/libraries/jquery/jquery.js') ?>"></script>
+    <script src="<?php echo base_url('/mainpages/libraries/bootstrap/bootstrap.js') ?>"></script>
+    <script src="<?php echo base_url('/mainpages/libraries/jquery/jquery.min.js') ?>"></script>
+    <script src="<?php echo base_url('/mainpages/libraries/bootstrap/bootstrap.min.js') ?>"></script>
+    <script src="<?php echo base_url('/mainpages/libraries/popper.js/popper.js') ?>"></script>
+    <script src="<?php echo base_url('/mainpages/libraries/perfect-scrollbar/js/perfect-scrollbar.jquery.js') ?>"></script>
+    <script src="<?php echo base_url('/mainpages/libraries/jquery-ui/jquery-ui.js') ?>"></script>
+    <script src="<?php echo base_url('/mainpages/libraries/jquery-switchbutton/jquery.switchButton.js') ?>"></script>
+    <script src="<?php echo base_url('/mainpages/libraries/peity/jquery.peity.js') ?>"></script>
+    <script src="<?php echo base_url('/mainpages/libraries/retina/retina.min.js') ?>"></script>
+    <script src="<?php echo base_url('/mainpages/libraries/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
+</body>
+
+</html>
